2454733c77e0a5977760e238cf03d13ef6bc52a0
Florin <florin.buga@gmail.com>
2020-05-08 11:39:45 +0300

    feat: implement navigation buttons

--- src/components/ShareholderDetailsPage/index.js
+++ src/components/ShareholderDetailsPage/index.js
@@ -1,20 +1,40 @@
 import React, { useState } from "react";
 
 const ShareholderDetailsPage = () => {
-  const [step, setStep] = useState(1);
+  const steps = [<AddShareholder />, <DivideShares />];
+
+  const [step, setStep] = useState(0);
+
+  const goBack = () => {
+    if (step > 0) {
+      setStep(step - 1);
+    }
+  };
+
+  const goToNext = () => {
+    if (step < steps.length - 1) {
+      setStep(step + 1);
+    }
+  };
 
   return (
     <main class="container">
       <h1>Shareholder Details</h1>
-      {step === 1 && <AddShareholder />}
-      {step === 2 && <DivideShares />}
-      <ControlButtons />
+      {steps[step]}
+      <NavButtons {...{ goBack, goToNext }} />
     </main>
   );
 };
 
 const AddShareholder = () => <div>AddShareholder</div>;
+
 const DivideShares = () => <div>DivideShares</div>;
-const ControlButtons = () => <div>ControlButtons</div>;
+
+const NavButtons = ({ goBack, goToNext }) => (
+  <nav className="nav-buttons">
+    <button onClick={goBack}>Back</button>
+    <button onClick={goToNext}>Next</button>
+  </nav>
+);
 
 export default ShareholderDetailsPage;
