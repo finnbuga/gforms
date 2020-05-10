a7d191fccec68f51d5b61d1b1de7e09f016dd5a0
Florin <florin.buga@gmail.com>
2020-05-10 23:53:26 +0300

    build: add Enzyme

---
+++ src/enzyme.js
@@ -0,0 +1,7 @@
+import Enzyme, { configure, mount, shallow, render } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+export { mount, shallow, render };
+export default Enzyme;
