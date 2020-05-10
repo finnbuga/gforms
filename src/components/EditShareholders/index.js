bb5cb1f484e36b00154f65fffc14e02ad0aed845
Florin <florin.buga@gmail.com>
2020-05-10 22:14:22 +0300

    style: reformat EditShreholders component

--- src/components/EditShareholders/index.js
+++ src/components/EditShareholders/index.js
@@ -5,25 +5,23 @@ import { useSelector, useDispatch } from "react-redux";
 
 import "./style.css";
 
-const EditShareholders = () => {
-  return (
-    <>
-      <div className="info">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-        <p>
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum.
-        </p>
-      </div>
+const EditShareholders = () => (
+  <>
+    <div className="info">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <p>
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum.
+      </p>
+    </div>
 
-      <EditShareholdersForm />
-    </>
-  );
-};
+    <EditShareholdersForm />
+  </>
+);
 
 const EditShareholdersForm = () => {
   const shareholders = useSelector((state) => state.shareholders);
